de443c55eb7f7443f06daa9de4c4d3a5f5a6ff05
loicbdev <loicbodolec@msn.com>
2020-12-28 13:00:15 +0100

    init

--- src/App.js
+++ src/App.js
@@ -4,65 +4,76 @@ import {
   Switch,
   Route,
   Link,
+  Redirect,
+  useParams,
   useRouteMatch
 } from "react-router-dom";
 
-// This example show how you could create a custom
-// <Link> that renders something special when the URL
-// is the same as the one the <Link> points to.
+// Sometimes you don't know all the possible routes
+// for your application up front; for example, when
+// building a file-system browsing UI or determining
+// URLs dynamically based on data. In these situations,
+// it helps to have a dynamic router that is able
+// to generate routes as needed at runtime.
+//
+// This example lets you drill down into a friends
+// list recursively, viewing each user's friend list
+// along the way. As you drill down, notice each segment
+// being added to the URL. You can copy/paste this link
+// to someone else and they will see the same UI.
+//
+// Then click the back button and watch the last
+// segment of the URL disappear along with the last
+// friend list.
 
-export default function CustomLinkExample() {
+export default function RecursiveExample() {
   return (
     <Router>
-      <div>
-        <OldSchoolMenuLink
-          activeOnlyWhenExact={true}
-          to="/"
-          label="Home"
-        />
-        <OldSchoolMenuLink to="/about" label="About" />
-
-        <hr />
-
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route path="/:id">
+          <Person />
+        </Route>
+        <Route path="/">
+          <Redirect to="/0" />
+        </Route>
+      </Switch>
     </Router>
   );
 }
 
-function OldSchoolMenuLink({ label, to, activeOnlyWhenExact }) {
-  let match = useRouteMatch({
-    path: to,
-    exact: activeOnlyWhenExact
-  });
+function Person() {
+  let { url } = useRouteMatch();
+  let { id } = useParams();
+  let person = find(parseInt(id));
 
   return (
-    <div className={match ? "active" : ""}>
-      {match && "> "}
-      <Link to={to}>{label}</Link>
+    <div>
+      <h3>{person.name}’s Friends</h3>
+
+      <ul>
+        {person.friends.map(id => (
+          <li key={id}>
+            <Link to={`${url}/${id}`}>{find(id).name}</Link>
+          </li>
+        ))}
+      </ul>
+
+      <Switch>
+        <Route path={`${url}/:id`}>
+          <Person />
+        </Route>
+      </Switch>
     </div>
   );
 }
 
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
+const PEEPS = [
+  { id: 0, name: "Michelle", friends: [1, 2, 3] },
+  { id: 1, name: "Sean", friends: [0, 3] },
+  { id: 2, name: "Kim", friends: [0, 1, 3] },
+  { id: 3, name: "David", friends: [1, 2] }
+];
 
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
+function find(id) {
+  return PEEPS.find(p => p.id === id);
 }
